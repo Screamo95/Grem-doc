7484bcd4a34108ff226da79251cb7d71c32dd96f
Screamo95 <bykovglobal@gmail.com>
2024-10-24 16:45:29 +0300

    add job

--- js/script.js
+++ js/script.js
@@ -33,3 +33,33 @@ const product_swiper = new Swiper('.product .swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+// Выпадающее меню job
+
+document.querySelector(`.job__btn`).onclick = function() {
+  document.querySelector(`.job__select`).classList.toggle(`job__select_close`)
+}
+
+document.querySelector(`.job__select-link_1`).onclick = function() {
+  document.querySelector(`.job__select`).classList.add(`job__select_close`)
+  document.querySelector(`.job__btn-txt`).textContent = `Наше производство`
+  document.querySelector(`.job__box_1`).classList.remove(`job__box_close`)
+  document.querySelector(`.job__box_2`).classList.add(`job__box_close`)
+  document.querySelector(`.job__box_3`).classList.add(`job__box_close`)
+}
+
+document.querySelector(`.job__select-link_2`).onclick = function() {
+  document.querySelector(`.job__select`).classList.add(`job__select_close`)
+  document.querySelector(`.job__btn-txt`).textContent = `Лесоуправление и заготовка`
+  document.querySelector(`.job__box_2`).classList.remove(`job__box_close`)
+  document.querySelector(`.job__box_1`).classList.add(`job__box_close`)
+  document.querySelector(`.job__box_3`).classList.add(`job__box_close`)
+}
+
+document.querySelector(`.job__select-link_3`).onclick = function() {
+  document.querySelector(`.job__select`).classList.add(`job__select_close`)
+  document.querySelector(`.job__btn-txt`).textContent = `Питомник`
+  document.querySelector(`.job__box_3`).classList.remove(`job__box_close`)
+  document.querySelector(`.job__box_1`).classList.add(`job__box_close`)
+  document.querySelector(`.job__box_2`).classList.add(`job__box_close`)
+}
